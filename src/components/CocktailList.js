ce74d3f849c8713de97dfa93303ea42a115a2fa2
Cameron Shimmin <cameronshimmin@Camerons-Mac-mini.local>
2020-09-18 15:07:12 -0700

    finish

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React from 'react';
+import Cocktail from './Cocktail';
 
-export default function CocktailList() {
-  return <h1>cocktailList component</h1>;
+export default function CocktailList({ cocktails, loading }) {
+  if (loading) {
+    return <h2 className='section-title'>loading...</h2>;
+  }
+  if (cocktails.length < 1) {
+    return (
+      <h2 className='section-title'>
+        no cocktails matched your search criteria
+      </h2>
+    );
+  }
+  return (
+    <section className='section'>
+      c<h2 className='section-title'>cocktails</h2>
+      <div className='cocktails-center'>
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
 }
